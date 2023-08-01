76bda089f1096ddaae2e4c4d6d95a0ca950a1135
wheaney <42350981+wheaney@users.noreply.github.com>
2023-08-01 13:36:41 -0700

    Fix package naming for easier imports, replace hyphen with underscore

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ def generate_long_description_file():
 
 
 setup(
-    name='giant-dipper',
+    name='giant_dipper',
     version='0.1.1',
-    packages=['giant-dipper'],
+    packages=['giant_dipper'],
     install_requires=['robin-stocks', 'PyYAML', 'pyotp'],
     url='https://github.com/wheaney/giant-dipper',
     license='GPL-3.0-only',
